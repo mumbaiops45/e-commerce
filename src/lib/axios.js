1ef853867ecdf62df7c90234a799bc8ad2c63b2c
mumbaiops45 <mumbaiops45@gmail.com>
2026-06-20 17:51:02 +0530

    20/06

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,4 +5,13 @@ const api = axios.create({
     withCredentials: true,
 });
 
+// Automatically attach token to every request
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default api;
